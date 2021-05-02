6e243037d2ce7be421fa6c7ecca4dd64841a1c31
Katie McDonagh <mcdonagh.katie@outlook.com>
2021-05-02 14:37:21 +0100

    add pop up to marker!

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -5,12 +5,13 @@ interface Mappable {
     lng: number
   }
 }
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
   constructor(divId: string) {
     this.googleMap = new google.maps.Map(document.getElementById(divId), {
-      zoom: -5,
+      zoom: 1,
       center: {
         lat: 0,
         lng: 0
@@ -19,12 +20,20 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng
       }
+    })
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: 'Hi there!'
+      })
+
+      infoWindow.open(this.googleMap, marker)
     })
   }
 }
